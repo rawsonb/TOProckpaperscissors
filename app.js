fba5f4f14ab7a35fc0ebe99bc1a060642a74229c
f.blackmon <f.rawsonblackmon@gmail.com>
2022-10-29 20:00:15 -0400

    add getComputerChoice function

--- app.js
+++ app.js
@@ -1,1 +1,25 @@
-console.log("test")
+function getComputerChoice() {
+    let selection = randInt(1,4);
+    if (selection == 1){
+        return "rock";
+    }
+    if (selection == 2){
+        return "paper";
+    }
+    if (selection == 3){
+        return "scissors";
+    }
+}
+
+function playRound() {
+    console.log("welcome to rock paper scissors!");
+    playerChoice = prompt("rock, paper, or scissors?").toLowerCase();
+    console.log(`computer's choice: ${getComputerChoice()}`);
+}
+
+// min inclusive, max non-inclusive
+function randInt(min, max) { 
+    return Math.floor(Math.random() * (max-min) + min);
+}
+
+console.log(getComputerChoice());
